85154e9305596168b95ec972934bd7c6375164cf
naveen-pasham <naveenpashamofficial@gmail.com>
2024-02-28 13:37:33 +0530

    private key moved appgh

---
+++ app.js
@@ -0,0 +1,76 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const sequelize=require('./util/database');
+const cors=require('cors');
+const dotenv = require('dotenv');
+const fs=require('fs');
+
+
+const helmet=require('helmet');
+const Compression=require('compression');
+const morgan=require('morgan');
+
+
+
+dotenv.config();
+
+const User=require('./models/signup');
+const Expense=require('./models/addexpense');
+const Order = require('./models/orders');
+const Forgotpassword = require('./models/forgotpassword');
+
+const app = express();
+
+const userRoutes = require('./routes/signup');
+const expenseRoutes = require('./routes/addexpense');
+const purchaseRoutes = require('./routes/purchase');
+const premiumRoutes = require('./routes/premium');
+ const forgotpasswordRoutes = require('./routes/forgotpassword');
+
+ app.use(helmet());
+ app.use(Compression());
+ 
+const accessLogStream=fs.createWriteStream(
+  path.join(__dirname,'access.log'),
+  {flags:'a'}
+);
+ 
+app.use(morgan('combined',{stream:accessLogStream}));
+
+app.use(cors());
+app.use(bodyParser.json({ extended: false }));
+
+app.use('/user', userRoutes);
+app.use('/userexpense', expenseRoutes);
+app.use('/purchase', purchaseRoutes);
+app.use('/premium', premiumRoutes);
+app.use('/password', forgotpasswordRoutes);
+
+User.hasMany(Expense);
+Expense.belongsTo(User);
+
+User.hasMany(Order);
+Order.belongsTo(User);
+
+User.hasMany(Forgotpassword);
+Forgotpassword.belongsTo(User);
+
+
+
+app.use(errorController.get404);
+
+sequelize
+// .sync({alter: true})
+// .sync({force: true})
+.sync()
+  .then(result => {
+   // console.log(result)
+    app.listen(process.env.PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
