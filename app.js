f9ca03b7d47e807fcb2e5abed5c6ec27b5ccbda3
naveen-pasham <naveenpashamofficial@gmail.com>
2024-03-04 23:48:10 +0530

    changed headers

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use((req,res,next)=>{
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader('Access-Control-Allow-Methods','GET,POST,HEAD,OPTIONS,PUT,PATCH,DELETE');
   res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
-  response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next(); 
 })
 
