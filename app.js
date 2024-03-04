7cbc5c7e1944168d1b2383132cac6471306e6fda
naveen-pasham <naveenpashamofficial@gmail.com>
2024-03-04 23:38:58 +0530

    adding cors to headers

--- app.js
+++ app.js
@@ -40,6 +40,12 @@ const accessLogStream=fs.createWriteStream(
 );
  
 app.use(morgan('combined',{stream:accessLogStream}));
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+  next(); 
+})
 
 // app.use(function (req, res, next) {
 //   res.setHeader(
